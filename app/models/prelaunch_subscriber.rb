3bcfee06c9ee64c574736bfe2a9384dade4be6ac
Peihong Li <peihong.li@shopify.com>
2015-03-14 23:48:39 -0400

    subscriber model

---
+++ app/models/prelaunch_subscriber.rb
@@ -0,0 +1,8 @@
+class PrelaunchSubscriber < ActiveRecord::Base
+  validates_presence_of :email, message: 'Please enter your email address.'
+  validates_uniqueness_of :email, message: 'That email is already on the subscription list.'
+  validates_format_of email:,
+    with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i,
+    message: 'Please use a valid email address.'
+
+end
